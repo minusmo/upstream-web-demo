f70b1ac05378b7d719ac4491138276462157eca8
hojooneum <bldolphin96@gmail.com>
2020-07-16 17:47:40 +0900

    [UPDATE] TopAppBar added & test by component on dev branch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 import Home from "./Home";
+import TopAppBar from "./TopAppBar";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/SignIn" component={SignIn} />
         <Route path="/SignUp" component={SignUp} />
+        <Route path="/TopAppBar" component={TopAppBar} />
         <Redirect path="*" to="/" />
       </Switch>
     </BrowserRouter>
---
+++ src/TopAppBar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import AccountCircleRoundedIcon from "@material-ui/icons/AccountCircleRounded";
+import SearchRoundedIcon from "@material-ui/icons/SearchRounded";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function TopAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Hyundai
+          </Typography>
+          <SearchRoundedIcon />
+          <AccountCircleRoundedIcon />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
